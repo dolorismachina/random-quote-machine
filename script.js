e836edf692c27cfb0942c92f5a79270a2b6d3170
radek-desktop <dolorismachina@gmail.com>
2018-10-27 21:52:05 +0100

    Add fade animation
    Add fade animation to background and text when new quote is loaded.

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const quoteUrl = 'https://talaikis.com/api/quotes/random/';
 const imgAPI = 'https://source.unsplash.com/random/1280x720';
 
 window.addEventListener('load', fetchQuote());
+// Element references
+const bg1 = document.querySelector('#bg1');
+const bg2 = document.querySelector('#bg2');
+const quoteText = document.querySelector('.quote-text');
+const quoteAuthor = document.querySelector('.quote-author');
+
+addEventListeners();
+
 
 const getQuoteBtn = document.querySelector('.next-quote');
 getQuoteBtn.addEventListener('click', e => {
@@ -18,19 +26,17 @@ function fetchQuote() {
 
 function injectQuote(quoteData) {
   console.log('fetch')
-  const quoteText = document.querySelector('.quote-text');
-  const quoteAuthor = document.querySelector('.quote-author');
-  
   quoteText.textContent = `"${quoteData.quote}"`;
   quoteAuthor.textContent = quoteData.author;
+
+  quoteText.classList.add('fade-in');
 }
 
 function fetchBackground() {
   window.fetch(imgAPI)
     .then( res => {
       const imgProp = `url("${res.url}")`;
-      console.log(imgProp);
-      document.querySelector('.container').style.setProperty('--img', imgProp);
+      swapImages(imgProp);
 
       fetchQuote();
     })
@@ -39,4 +45,31 @@ function fetchBackground() {
 
 function getNewQuote() {
   fetchBackground();
+}
+function swapImages(newImg) {
+  
+  const oldImg = document.querySelector('#bg1').style.getPropertyValue('--img');
+  
+  bg2.style.setProperty('--img', oldImg);
+  bg1.style.setProperty('--img', newImg);
+
+  bg1.classList.add('fade-in');
+  bg2.classList.add('fade-out');
+
+  
+}
+
+
+function addEventListeners() {
+  bg1.addEventListener('animationend', e => {
+    bg1.classList.remove('fade-in');
+  });
+  
+  bg2.addEventListener('animationend', e => {
+    bg2.classList.remove('fade-out');
+  });
+
+  quoteText.addEventListener('animationend', e => {
+    quoteText.classList.remove('fade-in');
+  });
 }
